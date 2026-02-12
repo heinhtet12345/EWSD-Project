cf5c267b4f4d5c6884bc0e78478b28d8e6abdee4
Zin Min Tun <anormalducky090@gmail.com>
2026-02-12 21:56:22 +0630

    fixed token issues

--- backend/api/views.py
+++ backend/api/views.py
@@ -26,6 +26,8 @@ class LoginView(APIView):
 
             #generate token jwt
             refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
 
             # to get user roles
             groups = user.groups.all()
@@ -43,6 +45,8 @@ class LoginView(APIView):
                 "username": user.username,
                 "role": role,
                 "profile_image": profile_image,
+                "access": access_token,
+                "refresh": refresh_token,
             })
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- backend/project_setting/urls.py
+++ backend/project_setting/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-
+ 
     path('api/', include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
