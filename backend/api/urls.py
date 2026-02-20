0238de0ca13a6f53ef93f6c60a3c9ebdbe23ce67
Zin Min Tun <anormalducky090@gmail.com>
2026-02-20 21:06:36 +0630

    removed login logic from this branch

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,7 +4,6 @@ from .views import AddCategoryView
 from .views import ViewCategoryView 
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
     path('categories/add/', AddCategoryView.as_view(), name='add-category'),
     path('categories/view/', ViewCategoryView.as_view(), name='view-categories'),
 ]
