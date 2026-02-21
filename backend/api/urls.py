f98481e470f023bf745f5bde6d6670086437330c
Zin Min Tun <anormalducky090@gmail.com>
2026-02-21 15:25:32 +0630

    fixed some issues

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views.categories_views import AddCategoryView
-from .views.categories_views import ViewCategoryView 
+from .views.categories_views import ViewCategoryView
+from .views.auth_views import LoginView 
+
 
 urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
     path('categories/add/', AddCategoryView.as_view(), name='add-category'),
     path('categories/view/', ViewCategoryView.as_view(), name='view-categories'),
 ]
