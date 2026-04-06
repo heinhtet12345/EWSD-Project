63afdbf30e9eb7d391e9cf190a2f1acb07b4b9bd
Hein Htet Aung <ygn.heinhtet12345@gmail.com>
2026-04-06 22:21:18 +0630

    Added STA's Terms and Conditions

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const API_URL = "https://your-backend.onrender.com/api/";
+// const API_URL = "http://127.0.0.1:8000/api/";
 // https://vite.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: API_URL,
         changeOrigin: true,
       },
       "/closure-period": {
-        target: "http://localhost:8000",
+        target: API_URL,
         changeOrigin: true,
       },
       "/ideas": {
-        target: "http://localhost:8000",
+        target: API_URL,
         changeOrigin: true,
       },
       "/media": {
-        target: "http://localhost:8000",
+        target: API_URL,
         changeOrigin: true,
       },
     },
